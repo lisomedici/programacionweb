7dc1f488568f4ff8398b3e9a7f59bab3244eb197
lisomedici <lmedici@itba.edu.ar>
2023-06-03 19:26:39 -0300

    ggg
    entrega 3

---
+++ script.js
@@ -0,0 +1,44 @@
+let array1= [1,2,3]
+
+console.log(array1)
+console.log(array[0]);
+
+const ARRAY2 = [1]
+
+//Metodos de Array
+//pop: Elimina el último elemento
+array1.pop()
+
+//push: Añade un elemento al array
+
+array1.push(3,14)
+//shift: Elimina el primer elemento del array
+//unshift: Agrega un elemento al principio del array
+//indexOf: Indica la posicion del elemento que este buscando
+//find: puedo encontrar un valor que cumpla con una condición específica
+
+let a= 0
+
+while(a>1){
+    console.log(a);console.log(b);
+}
+
+let result = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+
+console.log(result);
+// Expected output: "12345"
+
+for (let x=0; x<10; x++){
+    console.log(x);
+}
+
+let pantriste = [0,1,2,3]
+for(let x=0;x>4;x++){
+    console.log(pantriste[x])
+}
